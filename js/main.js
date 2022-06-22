7aa18a85b26d8615fb59a51e4dfeb86c5bbc79f2
irling <italodavidramosling@gmail.com>
2022-06-22 09:45:48 -0500

    se añadio codigo con redireccionamiento para las paginas

--- js/main.js
+++ js/main.js
@@ -1,45 +1,83 @@
 $(function(){
-   // GALERIA
-   $("#carousel-sites figure").append("<figcaption>");
+
+    //REDES REDIRECCIONAMIENTO
+
+    $("#redes figure").append("<figcaption>");
 
    // show/hide , fadeIn/fadeOut , slideDown/slideUp
 
-   $("#carousel-sites figure").mouseenter(function(){
+   $("#redes figure").mouseenter(function(){
        $(this).find("figcaption").stop().fadeIn("slow");
    });
 
-   $("#carousel-sites figure").mouseleave(function(){
+   $("#redes figure").mouseleave(function(){
        $(this).find("figcaption").stop().fadeOut("slow");
    });
 
-   $("#carousel-sites figure").each(function(){
+   $("#redes figure").each(function(){
+       var nombre = $(this).find("img").attr("title");
+       console.log(nombre);
+       var rutaImagen = $(this).find("img").attr("src");
+
+       $(this).find("figcaption").html("<div><h1 style='color: white;'>" + nombre + "</h1></div>");
+       $(this).find("figcaption div").append("<button type='button'; onclick='javascript:cambiar_redes();'><a>VER MÁS</a></button>");
+   });
+
+//BRANDING REDIRECCIONAMIENTO
+    $("#bran figure").append("<figcaption>");
+
+   // show/hide , fadeIn/fadeOut , slideDown/slideUp
+
+   $("#bran figure").mouseenter(function(){
+       $(this).find("figcaption").stop().fadeIn("slow");
+   });
+
+   $("#bran figure").mouseleave(function(){
+       $(this).find("figcaption").stop().fadeOut("slow");
+   });
+
+   $("#bran figure").each(function(){
        var nombre = $(this).find("img").attr("title");
        console.log(nombre);
        var rutaImagen = $(this).find("img").attr("src");
 
        $(this).find("figcaption").html("<div><h1 style='color: white;'>" + nombre + "</h1></div>");
        $(this).find("figcaption div").append("<button type='button'; onclick='javascript:cambiar_branding();'><a>VER MÁS</a></button>");
-       //prepend permite agregar un objeto al inicio de otro     
-
-    //    $(this).find("figcaption div i").click(function(){
-    //        $("body").append("<div id='fondo-oscuro'>");
-    //        $("#fondo-oscuro").append("<img src='" + rutaImagen + "'>");
-    //        $("#fondo-oscuro").append("<h3>" + nombre + "</h3>");
-    //        $("#fondo-oscuro").click(function(){
-    //            $(this).remove();
-    //        });
-    //    });
    });
+
+   //ALEX DEY REDIRECCIONAMIENTO
+
+      $("#alex figure").append("<figcaption>");
+
+      // show/hide , fadeIn/fadeOut , slideDown/slideUp
+   
+      $("#alex figure").mouseenter(function(){
+          $(this).find("figcaption").stop().fadeIn("slow");
+      });
+   
+      $("#alex figure").mouseleave(function(){
+          $(this).find("figcaption").stop().fadeOut("slow");
+      });
+   
+      $("#alex figure").each(function(){
+          var nombre = $(this).find("img").attr("title");
+          console.log(nombre);
+          var rutaImagen = $(this).find("img").attr("src");
+   
+          $(this).find("figcaption").html("<div><h1 style='color: white;'>" + nombre + "</h1></div>");
+          $(this).find("figcaption div").append("<button type='button'; onclick='javascript:cambiar_alex();'><a>VER MÁS</a></button>");
+      });
+
 });
 
 function cambiar_branding(){
     location.href="/pages/branding.html"
 }
 function cambiar_alex(){
-    location.href="/pages/redes.html"
+    location.href="/pages/alex-dey.html"
 }
 function cambiar_redes(){
-    location.href="/pages/alex-dey.html"
+    location.href="/pages/redes.html"
 }
 
 // window.onscroll = function () {
