ee62548bdabce13740fb68cdd871f308e9b820a4
irling <italodavidramosling@gmail.com>
2022-07-04 15:10:56 -0500

    modal OHMIODIO

--- js/branding.js
+++ js/branding.js
@@ -70,7 +70,7 @@ var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
     modal.style.display = "block";
-    modalImg.src = "/img/urimare-modal.png";
+    modalImg.src = "/img/modal-branding/ohmiodio-modal.png";
     modalImg.alt = this.alt;
     captionText.innerHTML = this.alt;
 }
